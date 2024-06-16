89b98f785786da0f8cb4285a54d2f86b011d6b82
Artube1991 <artube1991@gmail.com>
2024-06-16 23:49:53 +0300

    new experience

--- Week3/Day1/Exercise2.py
+++ Week3/Day1/Exercise2.py
@@ -13,6 +13,19 @@ def bark(self) -> None:
 def present_dog(dog: Dog) -> None:
     print(f"Dog name: {dog.name}. Height: {dog.height}")
 
+def get_biggest_dog
+
 def main():
     david_dog = Dog(name="Rex", height=50)
     present_dog(david_dog)
+
+    sarahs_dog = Dog("Teacup", 20)
+    present_dog(sarahs_dog)
+
+    if david_dog.height > sarahs_dog.height:
+        print(f"{david_dog.name} is bigger than {sarahs_dog.name}")
+    else:
+        print(f"{sarahs_dog.name} is bigger than {david_dog.name}")
+
+if __name__ == "__main__":
+    main()
---
+++ Week3/Day1/Exercise4.py
@@ -0,0 +1,26 @@
+class Zoo():
+    def __init__(self, zoo_name):
+        self.animals = []
+        self.zoo_name = "Ramat Gan Zoo"
+    
+    def add_animal(self, new_animal):
+        self.new_animal = self.animals.append(new_animal)
+
+    def get_animals(self):
+        print(self.animals)
+
+    def sell_animal(animal_sold):
+        animal_sold = self.animals.clear()
+
+    def sort_animals(self):
+        self.sort_animals = sorted(self)
+
+    def get_groups(self):
+        print(self.sort_animals)
+
+ramat_gan_safari = Zoo("giraffe")
+ramat_gan_safari.add_animal("giraffe")
+ramat_gan_safari.add_animal("baboon")
+ramat_gan_safari.get_animals()
+ramat_gan_safari.sort_animals()
+
