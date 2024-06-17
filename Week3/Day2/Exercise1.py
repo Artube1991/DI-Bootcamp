6ab71ac485bba9a6d2601f3e028cbaa1ac3bccc3
Artube1991 <artube1991@gmail.com>
2024-06-17 15:32:11 +0300

    first try

---
+++ Week3/Day2/Exercise1.py
@@ -0,0 +1,34 @@
+class Pets():
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+class Cat():
+    is_lazy = True
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def walk(self):
+        return f'{self.name} is just walking around'
+    
+class Siamese(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+class Bengal(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+class Chartreux(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+    
+all_cats = [Siamese, Bengal, Chartreux]
+sara_pets = Pets(Cat)
+sara_pets.walk()
+
