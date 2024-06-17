92e4d37122efe4bd151281653251609c87bea550
Artube1991 <artube1991@gmail.com>
2024-06-17 08:58:20 +0300

    i did several exercises

--- Week3/Day1/Exercise2.py
+++ Week3/Day1/Exercise2.py
@@ -3,29 +3,29 @@ class Dog:
         self.name = name
         self.height = height
 
-def bark(self) -> None:
-    print(f"{self.name} goes woof!")
+    def bark(self) -> None:
+        print(f"{self.name} goes woof!")
 
     def jump(self) -> None:
         jump_height = self.height * 2
         print(f"{self.name} jumps {jump_height} cm high!")
 
-def present_dog(dog: Dog) -> None:
-    print(f"Dog name: {dog.name}. Height: {dog.height}")
+    def present_dog(self) -> None:
+        print(f"Dog name: {self.name}. Height: {self.height}")
 
-def get_biggest_dog
+def get_biggest_dog(dogs: list[Dog]) -> Dog:
+    biggest_dog = max(dogs, key=lambda dog: dog.height)
+    return biggest_dog
 
 def main():
     david_dog = Dog(name="Rex", height=50)
-    present_dog(david_dog)
+    david_dog.present_dog()
 
     sarahs_dog = Dog("Teacup", 20)
-    present_dog(sarahs_dog)
+    sarahs_dog.present_dog()
 
-    if david_dog.height > sarahs_dog.height:
-        print(f"{david_dog.name} is bigger than {sarahs_dog.name}")
-    else:
-        print(f"{sarahs_dog.name} is bigger than {david_dog.name}")
+    biggest_dog = get_biggest_dog([david_dog, sarahs_dog])
+    print(f"{biggest_dog.name} is the biggest dog in town!")
 
 if __name__ == "__main__":
     main()
