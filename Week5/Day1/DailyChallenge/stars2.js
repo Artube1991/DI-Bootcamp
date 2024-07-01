446f96c174f8f80396166c41e8f2119b3c8c6054
Artube1991 <artube1991@gmail.com>
2024-07-02 00:17:13 +0300

    dc

---
+++ Week5/Day1/DailyChallenge/stars2.js
@@ -0,0 +1,12 @@
+for (let i = 1; i <= 1; i++) {
+    console.log("*");
+    for (let j = 1; j <= 2; j++) {
+      console.log("*".repeat(2 + j -1));
+    }
+  }
+  for (let i = 1; i <= 1; i++) {
+    console.log("****");
+    for (let j = 1; j <= 2; j++) {
+      console.log("*".repeat(5 + j - 1));
+    }
+  }
