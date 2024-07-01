e1a86ac9663b51a263c229b89b1b926264e34f35
Artube1991 <artube1991@gmail.com>
2024-07-01 19:56:46 +0300

    new exercises

---
+++ Week5/Day1/Exercise3.js
@@ -0,0 +1,7 @@
+let result = prompt('Type a number: ')
+let result_num = Number(result)
+typeof(result_num)
+console.log(typeof(result_num))
+while (result_num < 10) {
+    prompt('Type a number: ')
+}
