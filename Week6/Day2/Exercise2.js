874565ebb848b342ba261ae0e7e806cfb3adc98d
Artube1991 <artube1991@gmail.com>
2024-07-09 00:12:12 +0300

    xs2

---
+++ Week6/Day2/Exercise2.js
@@ -0,0 +1,21 @@
+const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+const ordinal = ["th","st","nd","rd"];
+
+colors.forEach((elem, index) => index === 0
+    ? console.log(index+1 + ordinal[1] + " choice is " + elem)
+    : index === 1
+    ? console.log(index+1 + ordinal[2] + " choice is " + elem)
+    : index === 2
+    ? console.log(index+1 + ordinal[3] + " choice is " + elem)
+    : console.log(index+1 + ordinal[0] + " choice is " + elem)
+)
+
+
+// colors.forEach((elem, index) = index > 3
+//     ? console.log(index+1 + ordinal[0] + " choice is " + elem)
+//     : index = 0
+//     ? console.log(index+1 + ordinal[1] + " choice is " + elem)
+//     : index = 1
+//     ? console.log(index+1 + ordinal[2] + " choice is " + elem)
+//     : index = 2
+// )
