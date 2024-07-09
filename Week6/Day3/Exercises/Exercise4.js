096c5d9c7696c083a60b7ed8d1dca647a635edb2
Artube1991 <artube1991@gmail.com>
2024-07-09 19:24:37 +0300

    xs4

---
+++ Week6/Day3/Exercises/Exercise4.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(name) {
+      this.naming = name;
+    }
+  }
+  
+  const member = new Person('John');
+  console.log(member);
+
+//   I think the type of "member" is an object
