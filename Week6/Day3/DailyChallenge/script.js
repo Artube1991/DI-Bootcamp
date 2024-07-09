d470683fc4f2950deb4c9083bfa8cf9c3a126008
Artube1991 <artube1991@gmail.com>
2024-07-09 23:10:26 +0300

    W6D3 DC

---
+++ Week6/Day3/DailyChallenge/script.js
@@ -0,0 +1,25 @@
+class Video {
+    constructor(title, uploader, time) {
+        this.title = title;
+        this.uploader = uploader;
+        this.time = time;
+    }
+watch() {
+    console.log(`${this.uploader} watched all ${this.time} of ${this.title}!`)
+}
+}
+
+video_watcher = new Video("This receipt just blows your brain! Just take a spoon...", "Yakubovich is Alive!", "500")
+video_watcher.watch()
+
+video_watcher_2 = new Video("LOL | Best Memes 2024", "FunnyBunny", "600")
+video_watcher_2.watch()
+
+video_watcher_3 = new Video("How to learn Python in 10 days", "Great Coder", "322")
+video_watcher_3.watch()
+
+video_watcher_4 = new Video("Interview of Moby for Israeli Radio", "AnimalRightsForever", "674")
+video_watcher_4.watch()
+
+video_watcher_5 = new Video("Truth about Migdal Haemek", "AnyaAndAlex", "1493")
+video_watcher_5.watch()
