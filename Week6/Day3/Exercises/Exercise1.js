d240b4677202c4746c4685772d6098c0afc31824
Artube1991 <artube1991@gmail.com>
2024-07-09 15:48:29 +0300

    w6d3 XS1

---
+++ Week6/Day3/Exercises/Exercise1.js
@@ -0,0 +1,16 @@
+const person = {
+    name: 'John Doe',
+    age: 25,
+    location: {
+        country: 'Canada',
+        city: 'Vancouver',
+        coordinates: [49.2827, -123.1207]
+    }
+}
+
+const {name, location: {country, city, coordinates: [lat, lng]}} = person;
+
+console.log(`I am ${name} from ${city}, ${country}. Latitude(${lat}), Longitude(${lng})`);
+
+// I think the output should be the string:
+// "I am John Doe from Vancouver, Canada. Latitude(49.2827), longitude(-123.1207)"
