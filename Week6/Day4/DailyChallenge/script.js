75ed641a704326dfac25464b8fcff532f35fb53a
Artube1991 <artube1991@gmail.com>
2024-07-14 01:06:23 +0300

    W6D4DC

--- Week6/Day4/DailyChallenge/script.js
+++ Week6/Day4/DailyChallenge/script.js
@@ -1,10 +1,16 @@
 document.querySelector("form").addEventListener("submit", getdata);
 
 function getdata(e) {
+    console.log(e.target.entries);
     e.preventDefault();
     const formData = new FormData(document.querySelector("form"));
-    const entries = data.entries();
-    for (entry of entries) {
-        console.log(entry)
-    }
+    console.log("formData:", formData);
+    const entries = formData.entries();
+    const info = Object.fromEntries(entries);
+    console.log("data:", info);
+    const toString = JSON.stringify(info);
+    console.log("toString:", toString);
+    const new_p = document.createElement("p");
+    new_p.innerText = toString;
+    document.body.appendChild(new_p);
 }
