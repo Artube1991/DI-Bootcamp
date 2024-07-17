ce1a96d99a521faf7b3ad862d22264380389a454
Artube1991 <artube1991@gmail.com>
2024-07-17 09:48:27 +0300

    something changed

--- Week7/Day2/Exercises/Exercise3.js
+++ Week7/Day2/Exercises/Exercise3.js
@@ -1,24 +1,20 @@
 async function starship() {
     let url = "https://www.swapi.tech/api/starships/9/";
-    // let options = {
-    //     method: "GET",
-    //     headers: {
-    //       "Content-type": "application:json",
-    //     },
-    // };
     
     const response = await fetch(url);
     if (response.ok) {
-      response.json() 
+      let data = await response.json();
+      properties = data.result.properties;
+      console.log(properties) 
     }
     else {
         throw new Error(`Oops! An error occurred: ${response.status}`)
     }
-   console.log(response)
+//    console.log(response)
 }
 
 starship().catch((e) => {
     console.log(`We have a problem here: ${e}`)
 })
 
-starship()
+// starship()
