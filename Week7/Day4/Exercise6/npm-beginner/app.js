c43f3274b882d1493ae72cb109b587072327e5ad
Artube1991 <artube1991@gmail.com>
2024-07-20 22:36:33 +0300

    w7d4 xs6

---
+++ Week7/Day4/Exercise6/npm-beginner/app.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+const log = console.log;
+
+log(chalk.blue("I'm blue, ") + chalk.red("da boo dee da boo dai!"))
+log(chalk.red("She comes in colors everywhere \n") +
+    // chalk.orange("She combs her hair \n") + 
+    chalk.yellow("She's like a rainbow \n") + 
+    chalk.green("Coming colors in the air \n") + 
+    chalk.blue("Oh, everywhere \n") 
+    // chalk.violet("She comes in colors \n")
+);
