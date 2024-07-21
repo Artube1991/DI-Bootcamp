2e40f58bc56eda78ac80e74e12e27f22476a45d9
Artube1991 <artube1991@gmail.com>
2024-07-21 23:02:08 +0300

    xs1

--- Week7/Day5/Exercises/Exercise1/server.js
+++ Week7/Day5/Exercises/Exercise1/server.js
@@ -20,3 +20,30 @@ app.listen(5000, () => {
     }
     res.json(item);
   });
+
+app.use(express.json());
+
+app.post("/content", (req, res) => {
+  const newItem = {
+    id: items.length + 1,
+    title: req.body.title,
+    content: req.body.content,
+  };
+  items.push(newItem);
+  res.status(201).json(newItem);
+});
+
+app.put("/content", (req, res) => {
+  const id = Number(req.params.itemID);
+  const index = items.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return res.status(404).send("Item not found");
+  }
+  const puttingItems = {
+    id: items[index].id,
+    title: req.body.title,
+    content: req.body.content,
+  };
+  content[index] = puttingItems;
+  res.status(200).json("Item updated");
+});
