e0f0b4cca2ec1544cb1003144b273a4871e3e45d
Artube1991 <artube1991@gmail.com>
2024-07-22 15:10:38 +0300

    xs2 continue

--- Week7/Day5/Exercises/Exercise2/app.js
+++ Week7/Day5/Exercises/Exercise2/app.js
@@ -9,3 +9,16 @@ app.listen(5000, () => {
 app.get("/api/books", (req, res) => {
     res.json(books);
   });
+
+app.get("/api/books/:bookID", (req, res) => {
+    const id = Number(req.params.bookID);
+    const book = books.find((book) => book.id === id);
+    
+
+    if (!book) {
+        return res.status(404).send("Book not found");
+    }
+
+    res.sendStatus(200);
+    res.json(book);
+})
