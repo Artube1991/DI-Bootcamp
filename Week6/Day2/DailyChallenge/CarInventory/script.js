e5c1c91e1492e880de633561dea5128ffc6e9fd0
Artube1991 <artube1991@gmail.com>
2024-07-27 16:27:26 +0300

    W6D2 DC completed

--- Week6/Day2/DailyChallenge/CarInventory/script.js
+++ Week6/Day2/DailyChallenge/CarInventory/script.js
@@ -1,8 +1,3 @@
-function getCarHonda(carInventory) {
-    let foundHonda = carInventory.find(item => item.car_make === "Honda");
-    console.log(`This is a ${foundHonda.car_make} ${foundHonda.car_model} from ${foundHonda.car_year}`);
-};
-
 const inventory = [
     { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
     { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
@@ -11,4 +6,19 @@ const inventory = [
     { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
   ];
 
+function getCarHonda(carInventory) {
+    let foundHonda = carInventory.find(item => item.car_make === "Honda");
+    console.log(`This is a ${foundHonda.car_make} ${foundHonda.car_model} from ${foundHonda.car_year}`);
+};
+
+
 getCarHonda(inventory);
+
+function sortCarInventoryByYear(carInventory) {
+let sortedCars = carInventory.sort(function (x, y) {
+    return x.car_year - y.car_year;
+  });
+console.log(sortedCars);
+};
+
+sortCarInventoryByYear(inventory);
