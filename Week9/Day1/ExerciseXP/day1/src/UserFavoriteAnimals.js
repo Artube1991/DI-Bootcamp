1d9b80eaf03da11836aa56519801f093609c1698
Artube1991 <artube1991@gmail.com>
2024-07-29 17:15:05 +0300

    w9d1xs2 completed

--- Week9/Day1/ExerciseXP/day1/src/UserFavoriteAnimals.js
+++ Week9/Day1/ExerciseXP/day1/src/UserFavoriteAnimals.js
@@ -2,20 +2,13 @@ import React from 'react';
 
 class Animals extends React.Component {
     render() {
-        console.log(this.props)
+        console.log(this.props);
         return this.props.favAnimals.map(animal => (
-            <ul>
+            <ul key={animal}>
                 <li>{animal}</li>
-                <li>{animal}</li>
-                <li>{animal}</li>
-            </ul>
+                </ul>
         ));
     }
-}
-
-// const Animals = (props) => {
-//     return <h2>Hi, I am a {props.favAnimals}!</h2>;
- 
-//  }
+};
 
 export default Animals;
