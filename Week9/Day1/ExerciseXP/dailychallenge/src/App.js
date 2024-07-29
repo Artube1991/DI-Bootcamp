3deee76c2758117a6ac36e6050f495a27e708540
Artube1991 <artube1991@gmail.com>
2024-07-29 20:32:53 +0300

    w9d1 dc complete

---
+++ Week9/Day1/ExerciseXP/dailychallenge/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { DemoCarousel } from './components/DemoCarouselComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <DemoCarousel />
+    </div>
+  );
+}
+
+export default App;
