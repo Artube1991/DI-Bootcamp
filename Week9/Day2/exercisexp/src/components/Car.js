122d419ea8658821989f267ec36f1520dda5fa21
Artube1991 <artube1991@gmail.com>
2024-07-30 17:08:06 +0300

    new

--- Week9/Day2/exercisexp/src/components/Car.js
+++ Week9/Day2/exercisexp/src/components/Car.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+// Part I
 const Car = (props) => {
     return <h1>This car is {props.carModel}</h1>
-
 }
+
+//Part II
+function CarColor() {
+    const [color, setColor] = useState("red");
+    return color
+  }
 
 export default Car
