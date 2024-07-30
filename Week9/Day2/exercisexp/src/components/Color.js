050054c36021f5f9f71672fec7f91e713cbde489
Artube1991 <artube1991@gmail.com>
2024-07-31 01:07:47 +0300

    xs 4 completed

--- Week9/Day2/exercisexp/src/components/Color.js
+++ Week9/Day2/exercisexp/src/components/Color.js
@@ -7,7 +7,13 @@ function Color() {
         setColor("yellow");
         alert("useEffect reached");
     });
-    return <header>My favourite color is <em>{favouriteColor}</em></header>
+    const changeColor = () => {
+        setColor("blue");
+    }
+    return <>
+    <header>My favourite color is <em>{favouriteColor}</em></header>
+    <button onClick={changeColor}>Change color to blue</button>
+    </>
 };
 
 export default Color
