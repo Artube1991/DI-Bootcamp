e8c83bd6260f104ed38cd83c476ae6603131cc77
Artube1991 <artube1991@gmail.com>
2024-08-01 09:39:24 +0300

    w9d4 start

--- Week9/Day4/exercise1/src/App.js
+++ Week9/Day4/exercise1/src/App.js
@@ -1,26 +1,39 @@
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import React from 'react';
+import { Routes, Route, Link, NavLink } from "react-router-dom";
+
+const Home = () => (
+    <h1>Home</h1>
+);
+
+const Profile = () => (
+    <h1>Profile Screen</h1>
+);
+
+const Shop = () => (
+    <h1>Shop</h1>
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+return (
+  <div>
+    <ul>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/profile">Profile</NavLink>
+      </li>
+      <li>
+        <NavLink to="/shop">Shop</NavLink>
+      </li>
+    </ul>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/shop" element={<Shop />} />
+    </Routes>
+  </div>
   );
-}
+};
 
 export default App;
