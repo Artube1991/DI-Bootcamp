c8891afb0f38a7e2e4002cb4c8fe9515b01ff596
Artube1991 <artube1991@gmail.com>
2024-08-05 00:35:20 +0300

    w9d5 dc 1/2

--- Week9/Day5/dailychallenge/src/App.js
+++ Week9/Day5/dailychallenge/src/App.js
@@ -1,12 +1,27 @@
+import React from 'react';
 import Math from './components/Math';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Math />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    }
+
+handleSubmit = event => {
+  let num1 = Number(event.target.num1.value);
+  let num2 = Number(event.target.num2.value);
+  let sum = num1 + num2;
+  return sum
 }
+
+render() {
+  return (
+    <div className="box" >
+      <Math handleSubmit={this.handleSubmit} />
+    </div>
+  )
+}
+}
+
 
 export default App;
--- Week9/Day5/dailychallenge/src/components/Math.js
+++ Week9/Day5/dailychallenge/src/components/Math.js
@@ -1,46 +1,27 @@
 import React from 'react';
 
-class Math extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            num1: "",
-            num2: ""
-        };
-    }
-
-    addNumbers = () => {
-        let { num1 } = this.props
-        let { num2 } = this.props
-        let userNumber = Number(e.target.num1.value);
-        let userNumber2 = Number(e.target.num2.value);
-        body.setAttribute('style', `background-color:${randomColor}`);
-        // quoteblock.setAttribute('style', `color:${colors[randIndCol]}`);
-        this.setState({ 
-            quote: randomQuote,
-            author: randomAuthor,
-            color: randomColor,
-         });
-
-        // moving();
-    }
-
-    render() {
-        return (
-            <>
-            <h1>Adding Two Numbers</h1>
-            <form onSubmit={this.addNumbers}>
-                <div>
-            <input type="number" className="input" name="num1" min="0" max="10"/>
-            </div>
-            <div>
-            <input type="number" className="input" name="num2" min="0" max="10"/>
-            </div>
-            <input type="submit" value="ADD" id="submit"/>
-                </form>
+const Math = (props) => {
+    let { handleSubmit } = props
+    return (
+        <>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input
+                    id="num1"
+                    name="num1"
+                    type="number"
+                    min="0" max="10000"
+                />
+                <input
+                    id="num2"
+                    name="num2"
+                    type="number"
+                    min="0" max="10000"
+                />
+                <button type="submit">ADD</button>
+            </form>
             </>
-        )
-    }
+
+    )
 }
 
 export default Math
