8475083b2b6c05bbfb22e36c89b4bf357796cdec
Artube1991 <artube1991@gmail.com>
2024-08-04 17:11:09 +0300

    dc last actions

--- Week9/Day5/miniproject/src/components/RandomQuotes.js
+++ Week9/Day5/miniproject/src/components/RandomQuotes.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import quotes from "./data.js";
 
+let quoteblock = document.getElementsByClassName("quoteblock")[0];
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
 }
 
+// function moving() {
+//     let start = Date.now();
+
+//     let timer = setInterval(function() {
+//       let timePassed = Date.now() - start;
+
+//       quoteblock.style.left = timePassed / 5 + 'px';
+
+//       if (timePassed > 1000) clearInterval(timer);
+
+//     }, 20);
+//   };
+
 let randIndxDef = getRandomInt(0, 94);
-let quoteblock = document.getElementsByClassName("quoteblock")[0];
 
 class Quotes extends React.Component {
     constructor(props) {
@@ -29,27 +43,16 @@ class Quotes extends React.Component {
             quote: randomQuote,
             author: randomAuthor,
          });
-        //  function moving() {
-        //     let start = Date.now();
-      
-        //     let timer = setInterval(function() {
-        //       let timePassed = Date.now() - start;
-      
-        //       quoteblock.style.up = timePassed / 5 + 'px';
-      
-        //       if (timePassed > 1000) clearInterval(timer);
-      
-        //     }, 20);
-        //   };
+
         // moving();
     }
 
     render() {
         return (
             <>
-            <p>{this.state.quote}</p>
-            <p>{this.state.author}</p>
-            <button type="button" onClick={this.chooseQuote}>New Quote</button>
+            <h2>"{this.state.quote}"</h2>
+            <p><right><em>--{this.state.author}</em></right></p>
+            <button type="button" id="button" onClick={this.chooseQuote}>New Quote</button>
             </>
         )
     }
