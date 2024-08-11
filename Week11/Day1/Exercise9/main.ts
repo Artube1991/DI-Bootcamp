ddc89298170142e8d948ea53de333ebc38b3b59c
Artube1991 <artube1991@gmail.com>
2024-08-11 20:17:38 +0300

    w11d1 xs9

---
+++ Week11/Day1/Exercise9/main.ts
@@ -0,0 +1,22 @@
+type Command = "run" | "quit" | "break";
+
+function getAction (command: Command): void {
+    switch (command) {
+      case "run":
+        console.log("I'm running the program.");
+        break;
+      case "quit":
+        console.log("I'm quiting the program.");
+        break;
+      case "break":
+        console.log("Let's take a 15 minutes break");
+        break;
+      default:
+        console.log("I don't know this command command");
+    }
+  };
+
+getAction("run");
+getAction("quit");
+getAction("break");
+getAction("sleep");
