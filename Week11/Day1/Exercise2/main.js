87ae03f36bc212efc11721334f3bcd065bda75bb
Artube1991 <artube1991@gmail.com>
2024-08-11 14:58:51 +0300

    w11d1 xs2

---
+++ Week11/Day1/Exercise2/main.js
@@ -0,0 +1,4 @@
+var first_name = 'Alex';
+console.log(first_name);
+var age = 17;
+console.log(age);
---
+++ Week11/Day1/Exercise2/main.ts
@@ -0,0 +1,5 @@
+let first_name: string = 'Alex';
+console.log(first_name);
+
+let age: number = 17;
+console.log(age);
