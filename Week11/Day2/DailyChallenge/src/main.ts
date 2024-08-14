0c61e2fcc767d54dd737d3098b49fb45ba2f4cef
Artube1991 <artube1991@gmail.com>
2024-08-14 16:00:13 +0300

    w11d2dc start

---
+++ Week11/Day2/DailyChallenge/src/main.ts
@@ -0,0 +1,9 @@
+function validateUnionType(value: any, allowedTypes: string[]): boolean {
+    for(const type of allowedTypes){
+        if(typeof value === type) {
+            return true
+        }
+        return false
+    }
+}
+
