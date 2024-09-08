fe73335d9dfb907056c2432e6e25a5d005ff6b92
Artube1991 <artube1991@gmail.com>
2024-09-08 19:56:21 +0300

    created routes

--- Week9/Day5/miniproject-snapshot/src/App.js
+++ Week9/Day5/miniproject-snapshot/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>SnapShot</h1>
+      <form className="searchbox">
+        <input type="text" placeholder="Find any picture you want!" />
+        <button type="submit" className="searchbutton">
+        <svg height="32" width="32">
+          <path
+          d="M19.427 21.427a8.5 8.5 0 1 1 2-2l5.585 5.585c.55.55.546 1.43 0 1.976l-.024.024a1.399 1.399 0 0 1-1.976 0l-5.585-5.585zM14.5 21a6.5 6.5 0 1 0 0-13 6.5 6.5 0 0 0 0 13z"
+          fill="#ffffff"
+          fill-rule="evenodd">
+            </path>
+          </svg>
+        </button>
+      </form>
+    <Link to="/mountains">Mountains   </Link>
+    <Link to="/beachs">Beachs   </Link>
+    <Link to="/birds">Birds   </Link>
+    <Link to="/food">Food</Link>
+    <Routes>
+      <Route path="/mountains" request="mountains"/>
+      <Route path="/beachs" request="beachs"/>
+      <Route path="/birds" request="birds"/>
+      <Route path="/food" request="food"/>
+    </Routes>
     </div>
   );
 }
