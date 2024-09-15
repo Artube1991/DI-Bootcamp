6ede7086e73efb435777ecb9fe69618e35875a84
Artube1991 <artube1991@gmail.com>
2024-09-16 00:43:06 +0300

    started with errorboundary

--- Week9/Day3/exercisesxp/src/App.js
+++ Week9/Day3/exercisesxp/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { useState } from 'react';
+import ErrorBoundary from './components/ErrorBoundary';
 
 class BuggyCounter extends React.Component {
   constructor(props) {
@@ -10,14 +11,19 @@ class BuggyCounter extends React.Component {
   handleClick = () => {
     let count = this.state.num;
     let setCount = count + 1;
-    this.setState({ num: setCount });
+    this.setState({ num: setCount })
+    if (setCount >= 5) {
+      throw new Error("I crashed!")
+    };
   };
 
   render() {
     return(
       <>
       <h1>Simulation 1</h1>
+      <ErrorBoundary>
       <div onClick={this.handleClick}>{this.state.num}</div>
+      </ErrorBoundary>
       </>
     )
   }
