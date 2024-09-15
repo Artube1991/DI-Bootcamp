9a2eb63128fb61084100a50491ddccc40c078cb3
Artube1991 <artube1991@gmail.com>
2024-09-15 23:59:07 +0300

    trying to create class component

--- Week9/Day3/exercisesxp/src/App.js
+++ Week9/Day3/exercisesxp/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import React, { useState } from 'react';
 
+const [count, setCount] = useState(0);
+
+class BuggyCounter extends React.Component {
+
+  render() {
+    return(
+      <>
+      <h1>Simulation 1</h1>
+      <div onClick={(e) => setCount(count + 1)}>{count}</div>
+      </>
+    )
+  }
+}
+
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-        <h1>Simulation 1</h1>
-        <div onClick={(e) => setCount(count + 1)}>{count}</div>
+
     </div>
   );
 }
