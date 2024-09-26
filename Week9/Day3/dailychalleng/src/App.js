026d6e471985fe0068863b56f495067775219654
Artube1991 <artube1991@gmail.com>
2024-09-26 19:39:57 +0300

    found the way to work with states in exercise

--- Week9/Day3/dailychalleng/src/App.js
+++ Week9/Day3/dailychalleng/src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import FormComponent from './components/FormComponent';
+import { useState } from 'react';
 
 const handleChange = (event) => {
   event.preventDefault();
   let data = event.target.getElementsByClassName('data');
-  for (const item of data) {
-    console.log(item.value)
-  }
+  console.log(data.firstName.value);
+  // for (const item of data) {
+  //   console.log(item.value)
+  // }
 };
 
 function App() {
+
+  const [firstName, setFirstName] = useState(``);
+  const [lastName, setLastName] = useState(``);
+  const [age, setAge] = useState(``);
+  const [gender, setGender] = useState(``);
+  const [destination, setDestination] = useState(``);
+  const [dietary, setDietary] = useState([]);
+
   return (
     <div className="App">
       <FormComponent handlesubmit={handleChange}/>
--- Week9/Day3/dailychalleng/src/components/FormComponent.js
+++ Week9/Day3/dailychalleng/src/components/FormComponent.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 const FormComponent = (props) => {
-    let { handlesubmit } = props;
+    // let { handlesubmit } = props;
     const [firstName, setFirstName] = useState(``);
     const [lastName, setLastName] = useState(``);
     const [age, setAge] = useState(``);
@@ -9,31 +9,47 @@ const FormComponent = (props) => {
     const [destination, setDestination] = useState(``);
     const [dietary, setDietary] = useState([]);
 
+    const handleChange = (event) => {
+        event.preventDefault();
+        let data = event.target.getElementsByClassName('data');
+
+        setFirstName(data.firstName.value);
+        setLastName(data.lastName.value);
+        setAge(data.age.value);
+        setGender(data.gender.value);
+        setDestination(data.destination.value);
+    }
+    
+
     return (
         <>
         <h1>Sample form</h1>
-        <form onSubmit={handlesubmit}>
-        <input className='data' type='text' placeholder='First Name'/> <br />
-        <input className='data' type='text' placeholder='Last Name'/> <br />
-        <input className='data' type='text' placeholder='Age'/> <br />
-        <input type="radio" id="male" name="male" value="Male" />
-        <label for="male">Male</label> <br />
-        <input type="radio" id="female" name="female" value="Female" />
-        <label for="female">Female</label><br />
-        <label for="cars">Select your destination</label> <br/>
-        <select id="destination" name="destination">
+        <form onSubmit={handleChange}>
+        <input className='data' type='text' name="firstName" placeholder='First Name'/> <br />
+        <input className='data' type='text' name="lastName" placeholder='Last Name'/> <br />
+        <input className='data' type='text' name="age" placeholder='Age'/> <br />
+        <input className='data' type="radio" id="gender" name="gender" value="Male" />
+        <label for="gender">Male</label> <br />
+        <input className='data' type="radio" id="gender" name="gender" value="Female" />
+        <label for="gender">Female</label><br />
+        <label for="destination">Select your destination</label> <br/>
+        <select className='data' id="destination" name="destination">
             <option value="Thailand">Thailand</option>
             <option value="Japan">Japan</option>
             <option value="Brazil">Brazil</option>
         </select> <br/>
-        <input type="checkbox" id="dietary1" name="dietary1" value="Nuts Free" />
+        <input className='data' type="checkbox" id="Nuts Free" name="Nuts Free" value="Yes" />
         <label for="Nuts Free"> Nuts Free</label><br />
-        <input type="checkbox" id="dietary2" name="dietary2" value="Lactose Free" />
-        <label for="dietary2"> Lactose Free</label><br />
-        <input type="checkbox" id="dietary3" name="dietary3" value="Vegan" />
-        <label for="dietary3"> Vegan</label><br/>
-        <button type="submit">SUBMIT</button>
+        <input className='data' type="checkbox" id="Lactose Free" name="Lactose Free" value="Yes" />
+        <label for="Lactose Free"> Lactose Free</label><br />
+        <input className='data' type="checkbox" id="Vegan" name="Vegan" value="Yes" />
+        <label for="Vegan"> Vegan</label><br/>
+        <button type='submit'>SUBMIT</button>
         </form>
+        <h1>Entered Information:</h1>
+        <p>Your name: {firstName} {lastName}</p>
+        <p>Your age: {age}</p>
+        <p>Your destination: {destination}</p>
         </>
     )
 };
