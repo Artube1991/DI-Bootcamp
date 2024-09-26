94572f8c7de3a72c7ec1c354351f1c148231d755
Artube1991 <artube1991@gmail.com>
2024-09-26 21:22:43 +0300

    ended up with backend

--- Week9/Day3/dailychalleng/src/App.js
+++ Week9/Day3/dailychalleng/src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
 import FormComponent from './components/FormComponent';
-import { useState } from 'react';
 
-const handleChange = (event) => {
-  event.preventDefault();
-  let data = event.target.getElementsByClassName('data');
-  console.log(data.firstName.value);
-  // for (const item of data) {
-  //   console.log(item.value)
-  // }
-};
 
 function App() {
 
   return (
     <div className="App">
-      <FormComponent handlesubmit={handleChange}/>
+      <FormComponent/>
     </div>
   );
 }
--- Week9/Day3/dailychalleng/src/components/FormComponent.js
+++ Week9/Day3/dailychalleng/src/components/FormComponent.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 
 const FormComponent = (props) => {
-    // let { handlesubmit } = props;
     const [firstName, setFirstName] = useState(``);
     const [lastName, setLastName] = useState(``);
     const [age, setAge] = useState(``);
@@ -11,27 +10,25 @@ const FormComponent = (props) => {
     const [lactose, setLactose] = useState('No');
     const [vegan, setVegan] = useState('No');
 
-    const handleChange = (event) => {
-        event.preventDefault();
-        let data = event.target.getElementsByClassName('data');
-        let genderButtons = document.getElementsByName('gender');
+    const handleChange = () => {
+        let data = document.getElementsByClassName('data');
+        // console.log(data);
+        // let genderButtons = document.getElementsByName('gender');
         let nutsButton = document.getElementsByName('Nuts Free')[0];
         let lactoseButton = document.getElementsByName('Lactose Free')[0];
         let veganButton = document.getElementsByName('Vegan')[0];
-        // let dietaryButtons = document.querySelector('input[type="checkbox"]')
-        console.log(genderButtons);
 
-        setFirstName(data.firstName.value);
-        setLastName(data.lastName.value);
-        setAge(data.age.value);
-        setGender(data.gender.value);
-        setDestination(data.destination.value);
-        for (let genButton of genderButtons) {
-            if (genButton.checked) {
-                setGender(genButton.value);
-            // genButton.checked ? setGender(genButton.value) : setGender(genButton.value)
+        // setFirstName(data.firstName.value);
+        // setLastName(data.lastName.value);
+        // setAge(data.age.value);
+        // setGender(data.gender.value);
+        // setDestination(data.destination.value);
 
-            }
+        // for (let genButton of genderButtons) {
+        //     if (genButton.checked) {
+        //         setGender(genButton.value);
+        //     genButton.checked ? setGender(genButton.value) : setGender(genButton.value)
+        //     }
 
         // if (nutsButton.checked) {
         //         setNuts(nutsButton.value);
@@ -40,38 +37,35 @@ const FormComponent = (props) => {
         //     setNuts('No');
         // }
         
-        nutsButton.checked == true ? setNuts('Yes') : setNuts('No')
-        lactoseButton.checked == true ? setLactose('Yes') : setLactose('No')
-        veganButton.checked == true ? setVegan('Yes') : setVegan('No')
+        nutsButton.checked === true ? setNuts('Yes') : setNuts('No')
+        lactoseButton.checked === true ? setLactose('Yes') : setLactose('No')
+        veganButton.checked === true ? setVegan('Yes') : setVegan('No')
         
-        }
-
-
-    }
+        };
     
 
     return (
         <>
         <h1>Sample form</h1>
-        <form onSubmit={handleChange}>
-        <input className='data' type='text' name="firstName" placeholder='First Name'/> <br />
-        <input className='data' type='text' name="lastName" placeholder='Last Name'/> <br />
-        <input className='data' type='text' name="age" placeholder='Age'/> <br />
-        <input className='data' type="radio" name="gender" value="Male" />
+        <form>
+        <input className='data' type='text' name="firstName" placeholder='First Name' onChange={(e) => setFirstName(e.target.value)} />  <br />
+        <input className='data' type='text' name="lastName" placeholder='Last Name' onChange={(e) => setLastName(e.target.value)} /> <br />
+        <input className='data' type='text' name="age" placeholder='Age' onChange={(e) => setAge(e.target.value)} /> <br />
+        <input className='data' type="radio" name="gender" value="Male" onChange={(e) => setGender(e.target.value)} />
         <label for="gender">Male</label> <br />
-        <input className='data' type="radio" name="gender" value="Female" />
+        <input className='data' type="radio" name="gender" value="Female" onChange={(e) => setGender(e.target.value)} />
         <label for="gender">Female</label><br />
         <label for="destination">Select your destination</label> <br/>
-        <select className='data' id="destination" name="destination">
+        <select className='data' id="destination" name="destination" onChange={(e) => setDestination(e.target.value)}>
             <option value="Thailand">Thailand</option>
             <option value="Japan">Japan</option>
             <option value="Brazil">Brazil</option>
         </select> <br/>
-        <input className='data' type="checkbox" id="Nuts Free" name="Nuts Free" value="Yes" />
+        <input className='data' type="checkbox" id="Nuts Free" name="Nuts Free" value="Yes" onChange={(e) => handleChange()}/>
         <label for="Nuts Free"> Nuts Free</label><br />
-        <input className='data' type="checkbox" id="Lactose Free" name="Lactose Free" value="Yes" />
+        <input className='data' type="checkbox" id="Lactose Free" name="Lactose Free" value="Yes" onChange={(e) => handleChange()}/>
         <label for="Lactose Free"> Lactose Free</label><br />
-        <input className='data' type="checkbox" id="Vegan" name="Vegan" value="Yes" />
+        <input className='data' type="checkbox" id="Vegan" name="Vegan" value="Yes" onChange={(e) => handleChange()}/>
         <label for="Vegan"> Vegan</label><br/>
         <button type='submit'>SUBMIT</button>
         </form>
