e4cfa6f6712fe6c42ff88f1c93d77cf84b161881
Artube1991 <artube1991@gmail.com>
2024-09-29 18:08:20 +0300

    working with controllers

--- Week8/Day1/ExercisesXP/Exercise2/routes/todos.js
+++ Week8/Day1/ExercisesXP/Exercise2/routes/todos.js
@@ -1,15 +1,50 @@
 const todos = [
     {id: 1,
-        task: "To make a haircut"},
+        todo: "To make a haircut"},
     {id: 2,
-        task: "To clean the bedroom"}, 
+        todo: "To clean the bedroom"}, 
     {id: 3,
-        task: "To finish discovering of Rondo Veneziano's discography"},
+        todo: "To finish discovering of Rondo Veneziano's discography"},
 ];
 
 const getTodos = (req, res) => {
     try {
       res.status(200).json(todos);
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({error: "Internal server error"})
+      }
+      };
+
+const createTask = (req, res) => {
+    try {
+    const { todo } = req.body;
+    const newTodo = {
+            id: todos.length + 1,
+            todo: todo
+        };
+        todos.push(newTodo);
+        res.status(201).json(newTodo);
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({error: "Internal server error"})
+      }
+      };
+
+const updateTask = (req, res) => {
+    try {
+        const { id, todo } = req.body; 
+        const index = todos.findIndex((todo) => todo.id === req.body.id);
+        if (index === -1) {
+          return res.status(404).send()
+        };
+        const newTodo = {
+          id: todos[index].id,
+          todo: req.body.todo,
+        };
+        todos.splice(index, 1, newTodo);
+        res.json(puttingTodo);
+        res.status(200).json("Task updated");
       } catch (error) {
         console.log(error);
         res.status(500).json({error: "Internal server error"})
