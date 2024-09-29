4ab36af655b5f2cd118ba9fbd78016f5376f9df0
Artube1991 <artube1991@gmail.com>
2024-09-29 19:01:57 +0300

    W8D1XS3 start

---
+++ Week8/Day1/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+// const { router } = require('./routes/index.js');
+
+app.listen(3000, () => {
+    console.log("This is how I listen to the server of Week 8 Day 1 Exercise 1");
+});
+
+// app.use('/', router);
