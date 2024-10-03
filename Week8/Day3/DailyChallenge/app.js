fea9919a1bebdef2bff9face268b8f41147db43e
Artube1991 <artube1991@gmail.com>
2024-10-03 14:45:27 +0300

    w8d3 dc start

---
+++ Week8/Day3/DailyChallenge/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const userRouter = require('./server/__routes/userRouter.js')
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`run on ${PORT}`);
+});
+
+app.use("/", userRouter);
