841c90f998f4f8b4bcd3f2f8f4d2d02a4a21389c
Artube1991 <artube1991@gmail.com>
2024-10-04 17:01:51 +0300

    found the way how to add tasks in the list

--- Week5/Day5DailyChallenge/script.js
+++ Week5/Day5DailyChallenge/script.js
@@ -1,13 +1,33 @@
-const tasks = ["Vanilla JavaScript", "Vue.js", "React.js", "Node.js"]
+const tasks = ["Vanilla JavaScript", "Vue.js", "React.js", "Node.js"];
 
-let taskBox =  document.getElementsByClassName("listTasks")[0]
+let listTasks =  document.getElementsByClassName("listTasks")[0];
+let form = document.getElementById("form");
+console.log(form);
 
-function addTask(todos) {
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e.target.input.value);
+    let newTask = e.target.input.value;
+    tasks.push(newTask);
+    console.log(tasks);
+    listTasks.innerHTML = "";
+    showTasks(tasks);
+});
+
+function showTasks(todos) {
     for (let todo of todos) {
         console.log(todo);
-        let newTask = document.createTextNode(todo);
-       taskBox.appendChild(newTask);
+        const task = document.createElement("p");
+        task.innerText = todo;
+        let deleteButton = document.createElement("button");
+        deleteButton.innerText = "✖";
+
+        let newTask = document.createElement("div");
+        newTask.appendChild(deleteButton);
+        newTask.appendChild(task);
+        listTasks.appendChild(newTask);
     }
 }
 
-addTask(tasks);
+showTasks(tasks);
