f19348a57705c0a02d4d96990d5de13ff9d7ed2b
Artube1991 <artube1991@gmail.com>
2024-10-10 17:50:14 +0300

    W7D5 DC complete

--- Week7/Day5/DailyChallenge/public/game.js
+++ Week7/Day5/DailyChallenge/public/game.js
@@ -1,14 +1,14 @@
 const url = "http://localhost:3000/emojis";
 
+const gameProcess = {
+    num: 1,
+};
+
 const randomEmoji = document.getElementById("emoji");
 const form = document.getElementById("form");
 const button = document.getElementById("button");
+const next = document.getElementById("next");
 const message = document.getElementById("message");
-
-const gameScore = {
-    total: 0,
-    score: 0,
-  }
 
 const game = (req, res) => {
     fetch(url)
@@ -21,37 +21,28 @@ const game = (req, res) => {
         console.log("The error is ", error);
     });
 
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        let answer = e.target.answer.value;
-        console.log(answer);
-        fetch(`http://localhost:3000/emojis/${answer}`, { 
-            method: "POST", 
-            headers: { "Accept": "application/json", "Content-Type": "application/json" },
-            params: answer,
-        })
-        .then(response => response.json())
-        .then(data => message.innerText = data);
-    });
 };
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let answer = e.target.answer.value;
+    console.log(answer);
+    fetch(`http://localhost:3000/emojis/${answer}`, { 
+        method: "POST", 
+        headers: { "Accept": "application/json", "Content-Type": "application/json" },
+        // params: answer,
+    })
+    .then(response => response.json())
+    .then(data => message.innerText = data);
+    console.log(gameProcess);
+});
 
-// const answer = (req, res) => {
-//     try {
-//         let answer = req.params.answer;
-//         if (answer === triviaQuestions[numberQuestion.number].answer) {
-//             gameScore.score++;
-//             res.status(201).json("Your answer is correct!")
-//         }
-//         else {
-//             res.status(404).json("You gave wrong answer!")
-//         }
-//         numberQuestion.number++;
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).json({error: "Internal server error"})
-//     }
-// }
+next.addEventListener("click", (e) => {
+    e.preventDefault();
+    gameProcess.num++;
+    game();
+    message.innerText = `The question number ${gameProcess.num}`;
+})
 
 
 game();
