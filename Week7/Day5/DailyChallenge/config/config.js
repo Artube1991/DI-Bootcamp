bdedf2128f122c34580c4418a491c755ecc100bd
Artube1991 <artube1991@gmail.com>
2024-10-10 13:31:33 +0300

    w7d5 DC start

---
+++ Week7/Day5/DailyChallenge/config/config.js
@@ -0,0 +1,14 @@
+const emojis = [
+    {emoji: "😀", name: "Smile"},
+    {emoji: "🐶", name: "Dog"},
+    { emoji: '🌮', name: 'Taco' },
+    { emoji: "🤣", name: "Lough"},
+    { emoji: "😡", name: "Angry"},
+    { emoji: "💁‍♂️", name: "Whatever"},
+];
+
+module.exports = {
+    emojis,
+};
+
+
