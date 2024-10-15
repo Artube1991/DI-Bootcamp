65d521f2c0242138159db4845757de68133dc932
Artube1991 <artube1991@gmail.com>
2024-10-15 12:15:12 +0300

    w5d4 xs2 complete

--- Week5/Day4/ExerciseXP/Exercise2/script.js
+++ Week5/Day4/ExerciseXP/Exercise2/script.js
@@ -1,14 +1,38 @@
+const body = document.getElementsByTagName("body")[0];
 const form = document.getElementsByTagName("form")[0];
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+const message = document.createElement("p");
+const usersAnswer = document.getElementsByClassName("usersAnswer")[0];
+
+body.appendChild(message);
 
 console.log(form);
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const fname = document.getElementById("fname");
+    
     console.log(fname.value);
-    const lname = document.getElementById("lname");
     console.log(lname.value);
+
     console.log(form.fname.value);
     console.log(form.lname.value);
-})
+
+    const list = document.createElement("li");
+    const list2 = document.createElement("li");
+
+    if (form.fname.value === "" || form.lname.value === "") {
+        message.innerText = "You didn't fill last name or first name fields!";
+    }
+    else {
+            usersAnswer.innerHTML = "";
+            message.innerText = "";
+            usersAnswer.appendChild(list);
+            list.innerText = form.fname.value;
+            usersAnswer.appendChild(list2);
+            list2.innerText = form.lname.value;
+    }
+});
+
+
 
